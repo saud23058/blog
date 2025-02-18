58eed062fa4d0a6872c37f38705d4e20cbf0094a
Saud_Khan <asaud3508@gmail.com>
2025-02-18 22:48:54 +0500

    changed the link in navbar

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -5,10 +5,10 @@ import React from "react";
 
 const Navbar = async () => {
   const session = await auth();
-
+  const { id} = session?.user || {};
   return (
     <nav className="w-full flex justify-between px-8 py-4 items-center">
-      <Link href="/">
+      <Link href={`/`}>
         <Image src="/Logo.png" alt="Logo" width={144} height={33} />
       </Link>
       <div className="flex items-center gap-4">
@@ -25,7 +25,7 @@ const Navbar = async () => {
            
             
              <Link
-  href={`/user/${session.user?.id}`}
+  href={`post/author-details/${id}`}
   className="h-12 w-12 flex justify-center items-center bg-blue-300 rounded-full overflow-hidden"
 >
   {session.user?.image ? (
